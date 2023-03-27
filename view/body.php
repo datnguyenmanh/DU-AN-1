6432932867694f251024ed1c2e41bfd68e07ae1e
datnguyenmanh <109351686+datnguyenmanh@users.noreply.github.com>
2023-03-27 15:04:33 +0700

    cắt giao diện sang php

---
+++ view/body.php
@@ -0,0 +1,74 @@
+<div id="wp-products">
+            <h2>Món ngon phải thử </h2>
+            <ul id="list-products">
+                <div class="item">
+                    <img class="product" src="view/assets/img/product_1.jpg" alt="">
+                    <div class="name">Món Ăn 1</div>
+                    <div class="desc">Mô Tả Ngắn Cho Sản Phẩm</div>
+                    <div class="price">500.000 VNĐ</div>
+                </div>
+
+                <div class="item">
+                    <img class="product" src="view/assets/img/product_2.png" alt="">
+                    <div class="name">Món Ăn 1</div>
+                    <div class="desc">Mô Tả Ngắn Cho Sản Phẩm</div>
+                    <div class="price">500.000 VNĐ</div>
+                </div>
+
+                <div class="item">
+                    <img class="product" src="view/assets/img/product_3.png" alt="">
+                    <div class="name">Món Ăn 1</div>
+                    <div class="desc">Mô Tả Ngắn Cho Sản Phẩm</div>
+                    <div class="price">500.000 VNĐ</div>
+                </div>
+
+                <div class="item">
+                    <img class="product" src="view/assets/img/product_4.png" alt="">
+                    <div class="name">Món Ăn 1</div>
+                    <div class="desc">Mô Tả Ngắn Cho Sản Phẩm</div>
+                    <div class="price">500.000 VNĐ</div>
+                </div>
+
+                <div class="item">
+                    <img class="product" src="view/assets/img/product_5.png" alt="">
+                    <div class="name">Món Ăn 1</div>
+                    <div class="desc">Mô Tả Ngắn Cho Sản Phẩm</div>
+                    <div class="price">500.000 VNĐ</div>
+                </div>
+
+                <div class="item">
+                    <img class="product" src="view/assets/img/product_6.png" alt="">
+                    <div class="name">Món Ăn 1</div>
+                    <div class="desc">Mô Tả Ngắn Cho Sản Phẩm</div>
+                    <div class="price">500.000 VNĐ</div>
+                </div>
+
+                <div class="item">
+                    <img class="product" src="view/assets/img/product_7.jpg" alt="">
+                    <div class="name">Món Ăn 1</div>
+                    <div class="desc">Mô Tả Ngắn Cho Sản Phẩm</div>
+                    <div class="price">500.000 VNĐ</div>
+                </div>
+
+                <div class="item">
+                    <img class="product" src="view/assets/img/product_8.jpg" alt="">
+                    <div class="name">Món Ăn 1</div>
+                    <div class="desc">Mô Tả Ngắn Cho Sản Phẩm</div>
+                    <div class="price">500.000 VNĐ</div>
+                </div>
+
+                <div class="item">
+                    <img class="product" src="view/assets/img/product_9.jpg" alt="">
+                    <div class="name">Món Ăn 1</div>
+                    <div class="desc">Mô Tả Ngắn Cho Sản Phẩm</div>
+                    <div class="price">500.000 VNĐ</div>
+                </div>
+
+                <div class="item">
+                    <img class="product" src="view/assets/img/product_10.jpg" alt="">
+                    <div class="name">Món Ăn 1</div>
+                    <div class="desc">Mô Tả Ngắn Cho Sản Phẩm</div>
+                    <div class="price">500.000 VNĐ</div>
+                </div>
+            </ul>
+        </div>
---
+++ view/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dự án 1</title>
+    <link rel="stylesheet" href="./view/assets/css/style.css">
+    <link rel="stylesheet" href="./view/assets/fonts/themify-icons/themify-icons.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+
+<body>
+    <div id="wrapper" class="navbar">
+        <div id="header">
+            <a class="navbar-brand" href="index.html">Liquor <span>store</span></a>
+            <ul id="menu">
+                <div class="item">
+                    <a href="#">Khuyến mại</a>
+                </div>
+                <div class="item">
+                    <a href="#">Thực đơn</a>
+                </div>
+                <div class="item">
+                    <a href="#">Theo dõi đơn hàng</a>
+                </div>
+                <div class="item">
+                    <a href="contact.html">Liên hệ</a>
+                </div>
+            </ul>
+            <div id="actions">
+                <div class="item">
+                    <a href="resgist.html">
+                        <i class="ti-user cart"></i>
+                    </a>
+                </div>
+                <div class="item">
+                    <a href="#">
+                        <i class="ti-shopping-cart cart"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
