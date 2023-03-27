71fc54d5598787615270eb4006a1f048ae1b3eba
datnguyenmanh <109351686+datnguyenmanh@users.noreply.github.com>
2023-03-27 14:34:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
 <?php
-include './page/website/header.php';
-if ((isset($_GET['url'])) && ($_GET['url'] != "")) {
-  $act = $_GET['url'];
-  switch ($act) {
-    case 'contact':
-      include "view/contact.php";
-      break;
-      case 'product':
-        include "view/product.php";
-        break;
-  }
-} else {
-  include "view/index.php";
-}
+  include "";
 
-include './page/website/footer.php';
+?>
